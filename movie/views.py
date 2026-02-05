3e2862e6bc67f5e4a15430da949213fc370281c6
AlysonH17 <alyson.uni2@gmail.com>
2026-02-05 18:27:59 -0500

    inicializacion modelos

--- movie/views.py
+++ movie/views.py
@@ -1,12 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import Movie
+
 # Create your views here.
 
 def home(request):
     #return HttpResponse('<h1>Welcome to Home Page</h1>')
     #return render(request, 'home.html')
-    return render(request, 'home.html', {'name' : 'Alyson Henao'})
+    #return render(request, 'home.html', {'name' : 'Alyson Henao'})
+    searchTerm = request.GET.get('searchMovie')
+    movies = Movie.objects.all()
+    return render(request, 'home.html', {'searchTerm': searchTerm, 'movies': movies})
 
 def about(request):
     return render(request, 'about.html')
